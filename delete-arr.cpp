727b55113cd197d33f6a4691d390c63ff7811530
DebanjanBarman <debanjanbarman123456789@gmail.com>
2019-09-16 07:05:48 +0530

    Change the for loop

--- delete-arr.cpp
+++ delete-arr.cpp
@@ -2,39 +2,34 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
 
-    int n,pos,i,arr[50],no;
+    int n, pos, i, arr[50];
 
     cout << "Enter the no of elements: ";
     cin >> n;
 
     cout << "Enter the Numbers: \n";
 
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
     cout << "\nDisplaying Output: \n\n";
 
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++) {
         cout << arr[i] << endl;
     }
 
     cout << "Enter the position where you want to delete: ";
     cin >> pos;
 
-    for (i = pos; i < n; i++)
-    {
+    for (i = pos; i < n; i++) {
         arr[i - 1] = arr[i];
     }
 
     cout << "\nFinal Output: \n\n";
-    for (i = n - 2; i >= 0; i--)
-    {
+    for (i = 0; i < n-1;  i++) {
         cout << arr[i] << endl;
     }
 }
