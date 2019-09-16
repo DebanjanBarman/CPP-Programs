4e3a14065b548990b74cfe0bdf13852bb5df8ae3
DebanjanBarman <debanjanbarman123456789@gmail.com>
2019-09-16 06:29:07 +0530

    no change

--- direct-indirect-recursion.cpp
+++ direct-indirect-recursion.cpp
@@ -1,29 +1,29 @@
 #include<iostream>
+
 using namespace std;
 int direct(int);
 int indirect(int);
 
-int main(){
+int main() {
 	int a;
-	cout<<"Enter the number: ";
-	cin>>a;
-	cout<<"Example of direct recursion\n\n";
+	cout << "Enter the number: ";
+	cin >> a;
+	cout << "Example of direct recursion\n\n";
 	direct(a);
-	cout<<"Example of indirect recursion\n\n";
+	cout << "Example of indirect recursion\n\n";
 	indirect(a);
 }
 
-int direct(int a){
-	
-	while(a>0){
-		cout<<a<<endl;
-		a-=1;
+int direct(int a) {
+
+	while (a > 0) {
+		cout << a << endl;
+		a -= 1;
 		direct(a);
 		break;
 	}
-	
+
 }
-int indirect(int a){
+int indirect(int a) {
 	direct(a);
 }
-
